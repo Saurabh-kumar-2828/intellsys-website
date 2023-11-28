5db20f99c3f3a6f38bd1f7b1711481edf06a0c21
lakshita-vyas <136328418+lakshita-vyas@users.noreply.github.com>
2023-11-28 12:55:00 +0530

    use healthCheck.json

--- app/routes/health-check.tsx
+++ app/routes/health-check.tsx
@@ -1,6 +1,32 @@
-export default function () {
+import {json, type LoaderFunction} from "@remix-run/node";
+import {useLoaderData} from "@remix-run/react";
+import healthCheck from "~/healthCheck.json";
+
+type LoaderData = {
+    deploymentTime: string;
+    commitId: string;
+};
+
+export const loader: LoaderFunction = async ({request}) => {
+    // TODO: Add encryption
+
+    const loaderData: LoaderData = {
+        deploymentTime: healthCheck.deploymentTime ?? null,
+        commitId: healthCheck.commitId ?? null,
+    };
+
+    return json(loaderData);
+};
+
+// TODO: Add meta to disable robot crawling
+
+export default function Component() {
+    const {deploymentTime, commitId} = useLoaderData() as LoaderData;
+
     return (
         <div>
-            COMMIT_ID        </div>
-    )
+            <div>{deploymentTime}</div>
+            <div>{commitId}</div>
+        </div>
+    );
 }
